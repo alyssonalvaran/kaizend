8f8d6c267d425c39bc9211edc0e43bedb672c34b
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-09-21 15:37:06 +0800

    Add Python Crash Course Exercises
    - Move python-refresher exercises to Python/1_basic folder

--- python/1_basic/refresher/exercise2.py
+++ python/1_basic/refresher/exercise2.py
@@ -7,8 +7,3 @@
 # "You are before or after the 21st century"
 
 year = 1830
-
-if(2000 < year > 2100):
-    print("Welcome to the 21st century")
-else:
-    print("You are before or after the 21st century")
