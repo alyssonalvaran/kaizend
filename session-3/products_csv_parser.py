e78369fcc40411f259dc61c6cc0b001856a7cb48
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 14:23:00 +0800

    Add products_csv_parser.py

---
+++ session-3/products_csv_parser.py
@@ -0,0 +1,11 @@
+# Exercise: Products CSV Reader and Writer
+
+# The main objective of this project idea is to read a CSV with a list
+# of products for an ecommerce site. The application should be able to:
+
+# - Prompt the user to specify the filename of the CSV input
+# - Remove all products that don’t have categories
+# - Output a new CSV file that contains all products
+#   that have categories
+# - Use argparse
+# - Name the exercise file products_csv_parser.py
