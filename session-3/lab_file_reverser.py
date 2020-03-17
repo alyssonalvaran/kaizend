62313c400f6e0e19cd4201c5e48fe8dd3da6494b
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 11:47:05 +0800

    Add business logic

--- session-3/lab_file_reverser.py
+++ session-3/lab_file_reverser.py
@@ -3,10 +3,17 @@ import argparse
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
-
-# add a --version flag
-# - use the action option to specify a string to print out when this flag is received
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
-print(args)
+
+# add the actual business logic for reversing the contents of the file
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+    
+    for line in lines:
+        print(line.strip()[::-1])
