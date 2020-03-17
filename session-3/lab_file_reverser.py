761a95a5d67ac4a8268cb804870e655a5adbc70b
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 12:09:48 +0800

    Add exit status

--- session-3/lab_file_reverser.py
+++ session-3/lab_file_reverser.py
@@ -1,4 +1,6 @@
 import argparse
+# add import for sys module
+import sys
 
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
@@ -7,14 +9,13 @@ parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
 
-# We utilize the try statement to denote that it’s quite possible for an error to happen within the try block
 try:
     f = open(args.filename)
     limit = args.limit
-# We can handle specific types of errors using the except keyword (we can have more than one).
 except FileNotFoundError as err:
     print(f"Error: {err}")
-# If there isn’t an error, we want to carry out the code that is in the else block
+    # set exit status to 1 to indicate error
+    sys.exit(1)
 else:
     with f:
         lines = f.readlines()
@@ -25,6 +26,3 @@ else:
         
         for line in lines:
             print(line.strip()[::-1])
-
-# If we want to execute some code regardless of there being an error or not,
-# we can put that in a finally block at the very end of our try/except code
