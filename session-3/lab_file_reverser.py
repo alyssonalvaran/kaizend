22698225d9a4368d2546969f1467ae8565a9895e
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 12:04:08 +0800

    Add error handlers

--- session-3/lab_file_reverser.py
+++ session-3/lab_file_reverser.py
@@ -7,13 +7,24 @@ parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
 
-# add the actual business logic for reversing the contents of the file
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
+# We utilize the try statement to denote that it’s quite possible for an error to happen within the try block
+try:
+    f = open(args.filename)
+    limit = args.limit
+# We can handle specific types of errors using the except keyword (we can have more than one).
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+# If there isn’t an error, we want to carry out the code that is in the else block
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+        
+        if args.limit:
+            lines = lines[:args.limit]
+        
+        for line in lines:
+            print(line.strip()[::-1])
 
-    if args.limit:
-        lines = lines[:args.limit]
-    
-    for line in lines:
-        print(line.strip()[::-1])
+# If we want to execute some code regardless of there being an error or not,
+# we can put that in a finally block at the very end of our try/except code
