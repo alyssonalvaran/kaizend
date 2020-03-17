7d05d97c10c4fa03c773fed4225daaccc6e3b5e3
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 11:44:32 +0800

    Add --version flag

--- session-3/lab_file_reverser.py
+++ session-3/lab_file_reverser.py
@@ -2,12 +2,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
-
-# add a --limit flag
-# - to specify that an argument is a flag, we need to place two hyphens at the beginning the flag’s name.
-# - we specified a shorter version of the flag as our second argument
-# - we used the type option for add_argument to state that we want the value converted to an integer
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+
+# add a --version flag
+# - use the action option to specify a string to print out when this flag is received
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
 print(args)
