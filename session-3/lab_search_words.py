862cca047b85d0c5269fd7c6a8e787099c24f834
Alysson Alvaran <alvaran.alysson@gmail.com>
2020-03-17 12:39:52 +0800

    Replace code with list comprehension

--- session-3/lab_search_words.py
+++ session-3/lab_search_words.py
@@ -6,13 +6,5 @@ parser.add_argument('snippet', help='partial (or complete) string to search for 
 args = parser.parse_args()
 snippet = args.snippet.lower()
 
-with open('/usr/share/dict/words') as f:
-    words = f.readlines()
-
-matches = []
-
-for word in words:
-    if snippet in word.lower():
-        matches.append(word)
-
-print(matches)
+words = open('/usr/share/dict/words').readlines()
+print([word for word in words if snippet in word.lower()])
